ab9a10a11bee408d7991cd8214ae54fe14328171
Saarah26 <saarahkashyap@gmail.com>
2022-07-01 21:25:53 +0530

    Short UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import {LogIn} from './components/LogIn';
+import {Short} from './components/Short'
 
 
 function App() {
   return (
     <>
-    <LogIn/>
+    <Short/>
     
     </>
   );
--- frontend/src/components/Url.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Url=()=> {
-  return (
-    <div>I hate aryamaan</div>
-  )
-}
