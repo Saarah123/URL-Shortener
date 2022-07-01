8d9916c28377caf1f4a78a056ca228377d06ff62
Saarah26 <saarahkashyap@gmail.com>
2022-07-01 11:08:02 +0530

    Log In

--- frontend/src/components/LogIn.js
+++ frontend/src/components/LogIn.js
@@ -1,20 +1,40 @@
 import React from 'react';
 import './login.css';
+import { useState} from 'react';
+
 
  export const LogIn=()=> {
+
+  const [user,setUser] = useState({
+    email:""
+  })
+
+  let name, value 
+  const handleInputs=(e)=>{
+       console.log(e)
+       name = e.target.name;
+       value=e.target.value;
+
+       setUser({...user,[name]:value})
+       
+  }
+
   return (
     <>
-    <div className='navbar' style={{maginLeft:"40%"}}>
-
+    <div className='navbar' style={{height:"50px", width:"100%", backgroundColor:"lavender"}}>
+<h1 style={{margin:"auto"}}>URL SHORTENER</h1>
     </div>
     <div className='mail'>
-        <form>
+        <form style={{marginLeft:"40%" , marginTop:"17%"}}  onSubmit={handleInputs}>
+
             <label><i>E-Mail</i></label><br/>
-            <input placeholder='Type here'></input>
-            <button>GO</button>
+
+            <input placeholder='Type here' name="email"  value={user.email}  onChange={handleInputs} ></input>
+
+           <button style={{marginLeft:"20px" , width:"50px", height:"20px", backgroundColor:"lavender"}}>GO</button>
         </form>
     </div>
     </>
   )
 }
-export default LogIn;
+
